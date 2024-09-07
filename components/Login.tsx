7344ca2f8a55592e13c5575864bc648747e5e637
Rana718 <ranadolui718@gmail.com>
2024-09-07 09:43:44 +0530

    use custom Buttom

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,27 +1,38 @@
 import { View, TouchableOpacity, Text, Image } from "react-native";
 import React from "react";
-
+import CustomButton from "./Custombutton";
 
 const Login = () => {
     return (
-        <View className="flex-1">
-            <Image source={require('@/assets/images/start_img.png')} className="w-full h-3/5" resizeMode="cover" />
-            <View className="flex-1 h-full rounded-t-3xl -top-14 bg-gray-900">
+        <View className="flex-1 bg-gray-900">
+            
+            <Image
+                source={require('@/assets/images/start_img.png')}
+                className="w-full h-3/5"
+                resizeMode="cover"
+                style={{
+                    borderBottomLeftRadius: 30,
+                    borderBottomRightRadius: 30,
+                    overflow: 'hidden',
+                }}
+            />
 
-                <Text className="text-white font-bold text-center text-4xl mt-10">
+            
+            <View className="flex-1 h-full rounded-t-3xl -top-14 bg-gray-800 px-8 py-10">
+                {/* App Title */}
+                <Text className="text-white font-bold text-center text-4xl mt-4">
                     TripGenius
                 </Text>
 
-                <Text className="text-white text-center mt-8 mx-10 text-sm">
+                
+                <Text className="text-gray-300 text-center mt-6 text-base leading-6">
                     Effortlessly plan your next adventure with personalized itineraries, smart recommendations, and real-time updates for stress-free, unforgettable travel experiences.
                 </Text>
 
-                <TouchableOpacity className="bg-red-400 rounded-3xl mx-10 mt-10">
-                    <Text className="text-gray-900 font-bold text-center text-2xl p-4">
-                        Sign with Google
-                    </Text>
-                </TouchableOpacity>
-
+                
+                <View className="flex-1 mt-4">
+                    <CustomButton containerStyle="px-28 py-4" onPress={()=>console.log("press")} title='Get Started'/>
+                </View>
             </View>
         </View>
     );
