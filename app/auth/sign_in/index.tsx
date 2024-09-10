7a8433d84e4b45210a95ae7772c3e8a74a5446cb
Rana718 <ranadolui718@gmail.com>
2024-09-10 19:36:21 +0530

    Update the auth

--- app/auth/sign_in/index.tsx
+++ app/auth/sign_in/index.tsx
@@ -1,9 +1,11 @@
-import { View, Text, TextInput, TouchableOpacity, StatusBar } from "react-native";
+import { View, Text, TextInput, TouchableOpacity, StatusBar, ToastAndroid, ScrollView } from "react-native";
 import React, { useEffect, useState } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { useRouter } from "expo-router";
 import CustomButton from "@/components/Custombutton";
 import AntDesign from '@expo/vector-icons/AntDesign';
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "@/config/FirebaseConfig";
 
 const SignIn = () => {
     const navigation = useNavigation();
@@ -17,72 +19,95 @@ const SignIn = () => {
         });
     }, [navigation]);
 
+    const handleSignIn = () => {
+        if (!email && !password) {
+            ToastAndroid.show('Please enter all fields', ToastAndroid.BOTTOM);
+            return;
+        }
+
+        signInWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+
+                ToastAndroid.show('Sign Up Successful!', ToastAndroid.BOTTOM);
+                router.replace('/mytrip');
+
+            })
+            .catch((error: any) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                ToastAndroid.show(errorMessage, ToastAndroid.BOTTOM);
+            });
+
+    }
+
     return (
         <View className="flex-1 px-8 pt-14 bg-white">
             <TouchableOpacity onPress={() => router.back()}>
                 <AntDesign name="arrowleft" size={32} color="black" />
             </TouchableOpacity>
 
-            <Text className="text-left text-4xl pt-2 font-bold text-blue-600">
-                Let's Sign You In
-            </Text>
-
-            {/* Subtitles */}
-            <Text className="text-left pt-2 text-2xl font-sans text-gray-600">
-                Welcome Back
-            </Text>
-            <Text className="text-left text-2xl font-sans text-gray-600">
-                You've been missed!
-            </Text>
-
-            {/* Input Fields */}
-            <View className="mt-10">
-                {/* Email Input */}
-                <Text className="text-gray-700 text-lg mb-2">Email</Text>
-                <TextInput
-                    placeholder="Enter your email"
-                    value={email}
-                    onChangeText={setEmail}
-                    className="border border-gray-300 rounded-lg px-4 py-3 text-base text-gray-700 bg-gray-100"
-                />
-
-                {/* Password Input */}
-                <Text className="text-gray-700 text-lg mt-6 mb-2">Password</Text>
-                <TextInput
-                    placeholder="Enter your password"
-                    value={password}
-                    onChangeText={setPassword}
-                    secureTextEntry
-                    className="border border-gray-300 rounded-lg px-4 py-3 text-base text-gray-700 bg-gray-100"
-                />
-            </View>
-
-            {/* Forgot Password */}
-            <TouchableOpacity onPress={() => console.log("purs")}>
-                <Text className="text-right mt-4 text-blue-600 font-semibold">
-                    Forgot Password?
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <Text className="text-left text-4xl pt-2 font-bold text-blue-600">
+                    Let's Sign You In
                 </Text>
-            </TouchableOpacity>
 
-            {/* Sign In Button */}
-            <View className="mt-10">
-                <CustomButton
-                    title="Sign In"
-                    onPress={() => console.log("purs")}
-                    textStyle="text-white text-center text-lg font-semibold"
-                    containerStyle="bg-blue-600 rounded-full py-4"
-                />
-            </View>
 
-            {/* Sign Up Link */}
-            <View className="flex-row justify-center mt-8">
-                <Text className="text-gray-600 text-base">Don't have an account? </Text>
-                <TouchableOpacity onPress={() => router.push('/auth/sign_up')}>
-                    <Text className="text-blue-600 text-base font-semibold">Sign Up</Text>
+                <Text className="text-left pt-2 text-2xl text-gray-600">
+                    Welcome Back
+                </Text>
+                <Text className="text-left text-2xl text-gray-600">
+                    You've been missed!
+                </Text>
+
+
+                <View className="mt-10">
+
+                    <Text className="text-gray-700 text-lg mb-2">Email</Text>
+                    <TextInput
+                        placeholder="Enter your email"
+                        value={email}
+                        onChangeText={setEmail}
+                        className="border border-gray-300 rounded-lg px-4 py-3 text-base text-gray-700 bg-gray-100"
+                    />
+
+
+                    <Text className="text-gray-700 text-lg mt-6 mb-2">Password</Text>
+                    <TextInput
+                        placeholder="Enter your password"
+                        value={password}
+                        onChangeText={setPassword}
+                        secureTextEntry
+                        className="border border-gray-300 rounded-lg px-4 py-3 text-base text-gray-700 bg-gray-100"
+                    />
+                </View>
+
+
+                <TouchableOpacity onPress={() => console.log("purs")}>
+                    <Text className="text-right mt-4 text-blue-600 font-semibold">
+                        Forgot Password?
+                    </Text>
                 </TouchableOpacity>
-            </View>
 
-            {/* Status Bar */}
+
+                <View className="mt-10">
+                    <CustomButton
+                        title="Sign In"
+                        onPress={handleSignIn}
+                        textStyle="text-white text-center text-lg font-semibold"
+                        containerStyle="bg-blue-600 rounded-full py-4"
+                    />
+                </View>
+
+
+                <View className="flex-row justify-center mt-8">
+                    <Text className="text-gray-600 text-base">Don't have an account? </Text>
+                    <TouchableOpacity onPress={() => router.push('/auth/sign_up')}>
+                        <Text className="text-blue-600 text-base font-semibold">Sign Up</Text>
+                    </TouchableOpacity>
+                </View>
+            </ScrollView>
+
+
             <StatusBar barStyle="dark-content" />
         </View>
     );
--- app/auth/sign_up/index.tsx
+++ app/auth/sign_up/index.tsx
@@ -1,11 +1,15 @@
-import { View, Text, TextInput, TouchableOpacity } from "react-native";
+import { View, Text, TextInput, TouchableOpacity, ToastAndroid, ScrollView } from "react-native";
 import React, { useEffect, useState } from "react";
-import { useRouter } from "expo-router";
+import { Redirect, useRouter } from "expo-router";
 import { useNavigation } from "expo-router";
 import AntDesign from '@expo/vector-icons/AntDesign';
 import CustomButton from "@/components/Custombutton";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "@/config/FirebaseConfig";
+
 
 const Sing_up = () => {
+
     const router = useRouter();
     const navigation = useNavigation();
     const [name, setName] = useState('');
@@ -18,61 +22,84 @@ const Sing_up = () => {
         });
     }, []);
 
+    const handleSignUp = () => {
+        if (!name && !email && !password) {
+            ToastAndroid.show('Please enter all fields', ToastAndroid.BOTTOM);
+            return;
+        }
+        createUserWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+
+                ToastAndroid.show('Sign Up Successful!', ToastAndroid.BOTTOM);
+                router.replace('/mytrip');
+
+            })
+            .catch((error: any) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                ToastAndroid.show(`Error: ${errorMessage}`, ToastAndroid.BOTTOM);
+
+            });
+    }
+
     return (
         <View className="flex-1 px-6 pt-14 bg-white">
             <TouchableOpacity onPress={() => router.back()}>
 
                 <AntDesign name="arrowleft" size={32} color="black" />
             </TouchableOpacity>
-            <Text className="text-left text-4xl font-bold text-blue-600 pt-3">
-                Create New Account
-            </Text>
+            <ScrollView showsVerticalScrollIndicator={false}>
 
-            <View className="mt-10">
+                <Text className="text-left text-4xl font-bold text-blue-600 pt-3">
+                    Create New Account
+                </Text>
 
-                <Text className="text-gray-700 text-lg mb-2">FullName</Text>
-                <TextInput
-                    placeholder="Enter your email"
-                    value={name}
-                    onChangeText={setName}
-                    className="border border-gray-300 rounded-lg px-4 py-3 text-base text-gray-700 bg-gray-100"
-                />
+                <View className="mt-10">
 
-                <Text className="text-gray-700 text-lg mb-2 mt-6">Email</Text>
-                <TextInput
-                    placeholder="Enter your email"
-                    value={email}
-                    onChangeText={setEmail}
-                    className="border border-gray-300 rounded-lg px-4 py-3 text-base text-gray-700 bg-gray-100"
-                />
+                    <Text className="text-gray-700 text-lg mb-2">FullName</Text>
+                    <TextInput
+                        placeholder="Enter your email"
+                        value={name}
+                        onChangeText={setName}
+                        className="border border-gray-300 rounded-lg px-4 py-3 text-base text-gray-700 bg-gray-100"
+                    />
+
+                    <Text className="text-gray-700 text-lg mb-2 mt-6">Email</Text>
+                    <TextInput
+                        placeholder="Enter your email"
+                        value={email}
+                        onChangeText={setEmail}
+                        className="border border-gray-300 rounded-lg px-4 py-3 text-base text-gray-700 bg-gray-100"
+                    />
 
 
-                <Text className="text-gray-700 text-lg mt-6 mb-2">Password</Text>
-                <TextInput
-                    placeholder="Enter your password"
-                    value={password}
-                    onChangeText={setPassword}
-                    secureTextEntry
-                    className="border border-gray-300 rounded-lg px-4 py-3 text-base text-gray-700 bg-gray-100"
-                />
-            </View>
+                    <Text className="text-gray-700 text-lg mt-6 mb-2">Password</Text>
+                    <TextInput
+                        placeholder="Enter your password"
+                        value={password}
+                        onChangeText={setPassword}
+                        secureTextEntry
+                        className="border border-gray-300 rounded-lg px-4 py-3 text-base text-gray-700 bg-gray-100"
+                    />
+                </View>
 
-            <View className="mt-12">
-                <CustomButton
-                    title="Sign Up"
-                    onPress={() => console.log("purs")}
-                    textStyle="text-white text-center text-lg font-semibold"
-                    containerStyle="bg-blue-600 rounded-full py-4"
-                />
-            </View>
-            <View className="mt-5">
-                <CustomButton
-                    title="Sign In"
-                    onPress={() => router.back()}
-                    textStyle="text-black text-center text-lg font-semibold"
-                    containerStyle="rounded-full border border-black py-4"
-                />
-            </View>
+                <View className="mt-12">
+                    <CustomButton
+                        title="Sign Up"
+                        onPress={handleSignUp}
+                        textStyle="text-white text-center text-lg font-semibold"
+                        containerStyle="bg-blue-600 rounded-full py-4"
+                    />
+                </View>
+                <View className="mt-5">
+                    <CustomButton
+                        title="Sign In"
+                        onPress={() => router.back()}
+                        textStyle="text-black text-center text-lg font-semibold"
+                        containerStyle="rounded-full border border-black py-4"
+                    />
+                </View>
+            </ScrollView>
 
         </View>
     );
