5ce3373e7cbc95429e387008e6543d675a1d9359
Rana718 <ranadolui718@gmail.com>
2024-09-10 23:40:49 +0530

    update the styling

--- components/SrartNewTripCard.tsx
+++ components/SrartNewTripCard.tsx
@@ -1,17 +1,22 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import React from "react";
 import Entypo from '@expo/vector-icons/Entypo';
 
-
-export default function SrartNewTripCard() {
-    return(
-        <View className="flex-1 p-20 m-50 items-center gap-2">
-            <Entypo name="location-pin" size={40} color="black" />
-            <Text className="text-2xl font-bold">No trips planned yet</Text>
-
-            <Text className="text-base text-gray-500">
-                Looks like its time to plan a new travel experinece! Get Started Below
+export default function StartNewTripCard() {
+    return (
+        <View className="flex-1 justify-center items-center p-10 bg-white rounded-lg shadow-md m-5">
+            <Entypo name="location-pin" size={50} color="red" />
+            <Text className="text-2xl font-bold text-center mt-4">
+                No trips planned yet
             </Text>
+            <Text className="text-base text-gray-500 text-center mt-2 px-5">
+                Looks like it's time to plan a new travel experience! Get started below.
+            </Text>
+            <TouchableOpacity className="p-3 bg-red-500 rounded-xl mt-7">
+                <Text className="text-white text-lg">
+                    Start a new trip
+                </Text>
+            </TouchableOpacity>
         </View>
-    )
+    );
 }
